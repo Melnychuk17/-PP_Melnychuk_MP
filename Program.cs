1232ef483455e52dfb476da0eae5e667fa0ebe1f
Helena <olenamelnychuk17@gmail.com>
2021-04-21 21:07:05 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace tcpp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {Console.WriteLine("Hello, world");
+        }
+    }
+}
