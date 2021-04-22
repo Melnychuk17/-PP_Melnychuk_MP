628533c2051356f4698c20646a04ed1856340a24
Melnychuk17 <60927001+Melnychuk17@users.noreply.github.com>
2021-04-22 11:42:50 +0300

    зміни з браузера

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace tcpp
     internal class Program
     {
         public static void Main(string[] args)
-        {Console.WriteLine("Hello, world");
+        {Console.WriteLine("Hello, world from browser");
             Console.WriteLine("Hello, people");
             Console.WriteLine("Hello, cat");
          Console.WriteLine("Hello, Donnu");
